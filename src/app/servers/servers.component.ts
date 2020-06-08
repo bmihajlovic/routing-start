7db0a20b08ea6d91a91df6c0b236834024253fee
bmihajlovic <bmih.git@gmail.com>
2020-06-08 08:29:40 +0200

    angular routing

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  private servers: {id: number, name: string, status: string}[] = [];
+
+  constructor(private serversService: ServersService,
+              private router: Router,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.servers = this.serversService.getServers();
+  } 
+
+  onReload(){
+    this.router.navigate(['servers', {relativeTo: this.route}]);   
+    // reload page  ....linije 3, 14, 20-21.........servers.component.html 13 linija
+  }
+
+}
